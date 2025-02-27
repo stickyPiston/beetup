b65865f448c12c407c1ce071b30f2cf31dada2bc
Job Vonk <sticcpistt@icloud.com>
2025-02-27 15:42:45 +0100

    Added logging in over http in frontend

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -4,11 +4,16 @@ import Browser
 import Browser exposing (UrlRequest, Document)
 import Browser.Navigation exposing (Key)
 
-import Html exposing (form, input, label, text, br)
+import Html exposing (Html, form, input, label, text, br)
 import Html.Events exposing (onSubmit, onInput)
 import Html.Attributes exposing (name, type_, value)
 
 import Url exposing (Url)
+
+import Http
+import Json.Decode as Decode exposing (Decoder, string)
+import Json.Decode.Pipeline exposing (required)
+import Json.Encode as Encode
 
 main : Program () Model Msg
 main = Browser.application
@@ -20,16 +25,34 @@ main = Browser.application
     , onUrlRequest = OnUrlRequest
     }
 
+type alias LoginForm =
+    { username : String
+    , password : String
+    }
+
 type alias Model =
     { count : Int
     , currentUrl : Url
-    , username : String
-    , password : String
+    , loginForm : LoginForm
+    , user : Maybe User
+    }
+
+type alias User =
+    { name : String
+    , id : String
     }
 
 init : () -> Url -> Key -> (Model, Cmd Msg)
 init () url _ = 
-    let model = { count = 0, currentUrl = url, password = "", username = "" }
+    let model =
+            { count = 0
+            , currentUrl = url
+            , loginForm =
+                { password = ""
+                , username = ""
+                }
+            , user = Nothing
+            }
      in (model, Cmd.none)
 
 type Msg
@@ -40,6 +63,7 @@ type Msg
     | LoginFormSubmit
     | UsernameChanged String
     | PasswordChanged String
+    | GotUser (Result Http.Error User)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -57,22 +81,58 @@ update msg model =
             , Cmd.none
             )
         OnUrlRequest _ -> (model, Cmd.none)
-        UsernameChanged new -> ({ model | username = new }, Cmd.none)
-        PasswordChanged new -> ({ model | password = new }, Cmd.none)
-        LoginFormSubmit -> Debug.log (Debug.toString model) (model, Cmd.none)
+        UsernameChanged new ->
+            let loginForm = model.loginForm
+                updatedLoginForm = { loginForm | username = new }
+             in ({ model | loginForm = updatedLoginForm }, Cmd.none)
+        PasswordChanged new ->
+            let loginForm = model.loginForm
+                updatedLoginForm = { loginForm | password = new }
+             in ({ model | loginForm = updatedLoginForm }, Cmd.none)
+        LoginFormSubmit ->
+            (model, Http.post
+                { body = Http.jsonBody (loginEncoder model.loginForm)
+                , url = "http://localhost:8001/login"
+                , expect = Http.expectJson GotUser userDecoder
+                })
+        GotUser response ->
+            ( response
+                |> Result.map (\user -> { model | user = Just user })
+                |> Result.withDefault model
+            , Cmd.none
+            )
+
+loginEncoder : LoginForm -> Encode.Value
+loginEncoder login = Encode.object 
+    [ ("username", Encode.string login.username)
+    , ("password", Encode.string login.password)
+    ]
+
+userDecoder : Decoder User
+userDecoder = Decode.succeed User
+    |> required "name" string
+    |> required "id" string
 
 view : Model -> Document Msg
 view model =
     { title = "Elm"
-    , body = 
-        [ form [onSubmit LoginFormSubmit]
-            [ label [] [text "Username: "]
-            , input [value model.username, type_ "text", name "username", onInput UsernameChanged] []
-            , br [] []
-            , label [] [text "Password: "]
-            , input [value model.password, type_ "password", name "password", onInput PasswordChanged] []
-            , br [] []
-            , input [type_ "submit"] []
-            ]
-        ]
+    , body = case model.user of
+        Just user -> viewLoggedIn user
+        Nothing -> viewLoginForm model.loginForm
     }
+
+viewLoggedIn : User -> List (Html Msg)
+viewLoggedIn user = [text ("Welcome " ++ user.name ++ "!")]
+
+viewLoginForm : LoginForm -> List (Html Msg)
+viewLoginForm loginForm =
+    [ form [onSubmit LoginFormSubmit]
+        [ label [] [text "Username: "]
+        , input [value loginForm.username, type_ "text", name "username", onInput UsernameChanged] []
+        , br [] []
+        , label [] [text "Password: "]
+        , input [value loginForm.password, type_ "password", name "password", onInput PasswordChanged] []
+        , br [] []
+        , input [type_ "submit"] []
+        ]
+    ]
