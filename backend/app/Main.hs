f2c15674790e00e93be1d18917f5206949773933
Job Vonk <sticcpistt@icloud.com>
2025-02-27 16:29:33 +0100

    Added simple database querying for login route

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -2,16 +2,30 @@ module Main where
 
 import Network.Wai.Handler.Warp (run)
 import Web.Twain
-import Network.HTTP.Types.Status (unauthorized401)
 import Data.Aeson
-import Data.Text (Text)
+import Data.Text (Text, pack)
+import Database.HDBC.Sqlite3 (connectSqlite3, Connection)
+import Database.HDBC (IConnection(commit, runRaw), quickQuery', toSql, fromSql)
+import Control.Monad (when)
+import Control.Monad.IO.Class (liftIO)
 
 main :: IO ()
 main = do
+  conn <- connectSqlite3 "main.db"
+  initDB conn
+
   run 8001 $
     foldr ($)
       (notFound missing)
-      [ post "/login" login ]
+      [ post "/login" (login conn) ]
+
+initDB :: Connection -> IO ()
+initDB conn = do
+  runRaw conn "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, name TEXT)"
+  [[fromSql -> userCount]] <- quickQuery' conn "SELECT COUNT(*) FROM users WHERE username = \"robin\"" []
+  when (userCount == (0 :: Int)) $ do
+    runRaw conn "INSERT INTO users (username, password, name) VALUES (\"robin\", \"hello\", \"Robin-Lynn\")"
+  commit conn
 
 data LoginParams = LoginParams
   { username :: Text
@@ -31,12 +45,16 @@ data LoginResponse = LoginResponse
 instance ToJSON LoginResponse where
   toJSON (LoginResponse name id) = object ["name" .= name, "id" .= id]
 
-login :: ResponderM a
-login = do
+login :: Connection -> ResponderM a
+login conn = do
   LoginParams username password <- fromBody
-  if username == "robin" && password == "hello"
-    then respond $ json $ LoginResponse "Robin-Lynn" "123"
-    else respond $ status unauthorized401 $ html ""
+  foundUsers <- liftIO $ quickQuery'
+    conn
+    "SELECT id, name FROM users WHERE username = ? AND password = ?"
+    [toSql username, toSql password]
+  case foundUsers of
+    [map fromSql -> [id, name]] -> respond $ json $ LoginResponse name (pack $ show id)
+    _ -> respond $ status unauthorized401 $ html ""
   where respond = send . withHeader ("Access-Control-Allow-Origin", "*")
 
 missing :: ResponderM a
