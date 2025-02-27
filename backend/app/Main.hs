9287f39ec5a4c98745f7e19d3f155c969963134c
Job Vonk <sticcpistt@icloud.com>
2025-02-27 17:10:53 +0100

    Added password hashing

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,20 +1,21 @@
 module Main where
 
-import Network.Wai.Handler.Warp (run)
+import qualified Network.Wai.Handler.Warp as Warp
 import Web.Twain
 import Data.Aeson
 import Data.Text (Text, pack)
 import Database.HDBC.Sqlite3 (connectSqlite3, Connection)
-import Database.HDBC (IConnection(commit, runRaw), quickQuery', toSql, fromSql)
+import Database.HDBC (IConnection(commit, runRaw), quickQuery', toSql, fromSql, run)
 import Control.Monad (when)
 import Control.Monad.IO.Class (liftIO)
+import Data.Password.Bcrypt
 
 main :: IO ()
 main = do
   conn <- connectSqlite3 "main.db"
   initDB conn
 
-  run 8001 $
+  Warp.run 8001 $
     foldr ($)
       (notFound missing)
       [ post "/login" (login conn) ]
@@ -24,7 +25,12 @@ initDB conn = do
   runRaw conn "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, name TEXT)"
   [[fromSql -> userCount]] <- quickQuery' conn "SELECT COUNT(*) FROM users WHERE username = \"robin\"" []
   when (userCount == (0 :: Int)) $ do
-    runRaw conn "INSERT INTO users (username, password, name) VALUES (\"robin\", \"hello\", \"Robin-Lynn\")"
+    hash <- hashPassword "hello"
+    _ <- run
+      conn
+      "INSERT INTO users (username, password, name) VALUES (\"robin\", ?, \"Robin-Lynn\")"
+      [toSql $ unPasswordHash hash]
+    return ()
   commit conn
 
 data LoginParams = LoginParams
@@ -47,13 +53,15 @@ instance ToJSON LoginResponse where
 
 login :: Connection -> ResponderM a
 login conn = do
-  LoginParams username password <- fromBody
+  LoginParams username (mkPassword -> password) <- fromBody
   foundUsers <- liftIO $ quickQuery'
     conn
-    "SELECT id, name FROM users WHERE username = ? AND password = ?"
-    [toSql username, toSql password]
+    "SELECT id, name, password FROM users WHERE username = ?"
+    [toSql username]
   case foundUsers of
-    [map fromSql -> [id, name]] -> respond $ json $ LoginResponse name (pack $ show id)
+    [map fromSql -> [id, name, PasswordHash -> pwHash]]
+      | checkPassword password pwHash == PasswordCheckSuccess ->
+        respond $ json $ LoginResponse name (pack $ show id)
     _ -> respond $ status unauthorized401 $ html ""
   where respond = send . withHeader ("Access-Control-Allow-Origin", "*")
 
